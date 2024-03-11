9a133ca95e0b7781396c4a3dd558514101983945
Kiran <160374868+kirankumarbehera8903@users.noreply.github.com>
2024-03-11 14:52:41 +0530

    Kiran_Cinema
    Using Html and CSS Javascript and Bootstrap

---
+++ kiran-hotstar/assets/js/data.js
@@ -0,0 +1,72 @@
+let movies = [
+  {
+    name: "Fighter",
+    des: "•Action •Adventure •Thriller  ⭐6.9/10",
+    image: "https://assets-in.bmscdn.com/discovery-catalog/events/et00304730-skwywnalwy-landscape.jpg",
+  },
+  {
+    name: "Dunki",
+    des: "•Comedy •Drama ⭐6.8",
+    image: "https://media.assettype.com/filmcompanion/2023-11/ce193cd9-3523-425e-b4e2-d909004dddad/Screenshot_2023_11_06_at_12_47_51.png?w=1200&h=675&auto=format%2Ccompress&fit=max&enlarge=true",
+  },
+  {
+    name: "Shaitaan",
+    des: "•Horror •Thriller ⭐7.8/10",
+    image: "https://www.comingsoon.net/wp-content/uploads/sites/3/2024/03/Coming-Soon-Cover-Images-2-3.png?w=1024",
+  },
+  {
+    name: "Animal",
+    des: "•Action •Crime •Drama ⭐6.3",
+    image: "https://img.indiaforums.com/media/640x0/59/4332-animal.jpg",
+  },
+  {
+    name: "Kalki 2898 AD",
+    des: "•Action •Adventure •Drama",
+    image: "https://images.yourstory.com/cs/2/96eabe90392211eb93f18319e8c07a74/Image5mxv-1689948556123.jpg?w=1152&fm=auto&ar=2:1&mode=crop&crop=faces",
+  },
+  {
+    name: "RRR",
+    des: "•Action •Adventure •Drama ⭐7.8",
+    image: "https://www.koimoi.com/wp-content/new-galleries/2022/03/rrr-beats-the-batman-the-lost-city-at-the-worldwide-box-office-this-weekend-01.jpg",
+  },
+  {
+    name: "KGF 2",
+    des: "•Action •Crime •Drama ⭐8.3",
+    image: "https://m.media-amazon.com/images/S/pv-target-images/631906bde1ca2b67bda7476df4e8c40767ec958003de46f8cb29bae88a086a29.jpg",
+  },
+  {
+    name: "Jawan",
+    des: "•Action •Drama •Musical ⭐7.0",
+    image: "https://img.onmanorama.com/content/dam/mm/en/entertainment/entertainment-news/images/2023/9/17/shah-rukh-khan-jawan-two.jpg",
+  },
+  {
+    name: "Pathaan",
+    des: "•Action •Adventure •Thriller ⭐5.8",
+    image: "https://www.koimoi.com/wp-content/new-galleries/2023/01/pathaan-box-office-day-2-morning-occupancy-01.jpg",
+  },
+  {
+    name: "Hanuman",
+    des: "•Action •Adventure •Fantasy ⭐8.4",
+    image: "https://m.economictimes.com/thumb/msid-106888809,width-1600,height-900,resizemode-4,imgsize-112166/hanumandb.jpg",
+  },
+  {
+    name: "Salaar",
+    des: "•Action •Crime •Drama ⭐6.5",
+    image: "https://www.nowrunning.com/content/movie/2021/salaa-25489/bg8_salaar.jpg",
+  },
+  {
+    name: "Leo",
+    des: "•Action •Crime •Drama ⭐7.2",
+    image: "https://static.toiimg.com/thumb/msid-101172246,width-1280,height-720,resizemode-4/101172246.jpg",
+  },
+  {
+    name: "Jailer",
+    des: "•Action •Comedy •Crime ⭐7.1",
+    image: "https://www.hindustantimes.com/ht-img/img/2023/08/19/1600x900/jailer_box_office_tamil_nadu_1692416829885_1692416841022.jpg",
+  },
+  {
+    name: "Yodha",
+    des: "•Action •Drama •Thriller",
+    image: "https://assets.thehansindia.com/h-upload/2021/11/18/1122369-yodha.webp",
+  },
+];
